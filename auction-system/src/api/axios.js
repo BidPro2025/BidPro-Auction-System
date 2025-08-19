7fd7b31c67838fa82b9346df36cf46c20ec03372
BidPro2025 <bidpro.sunbeam@gmail.com>
2025-08-20 01:49:22 +0530

    Add files via upload

---
+++ auction-system/src/api/axios.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { API_BASE } from '../utils/constants';
+
+const api = axios.create({
+  baseURL: API_BASE,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
